09ef31aec62d20cbbe7243bbeb93b684c60e9253
Thorsten Lorenz <thlorenz@gmx.de>
2013-05-01 22:53:31 -0400

    using domready in example app as well

--- examples/express-app/js/main.js
+++ examples/express-app/js/main.js
@@ -4,31 +4,36 @@
 // just requiring hyperwatch will initialize it and add a terminal to the browser window
 require('hyperwatch');
 
-var causeError =  document.getElementById('cause-error')
-  , getUser    =  document.getElementById('get-user')
-  , getUnknown =  document.getElementById('get-unknown')
-  , results    =  document.getElementById('results');
+var domready = require('domready');
 
-causeError.onclick = function () {
-  var req = new XMLHttpRequest();
-  req.open("GET", "/error");
-  req.send();
-};
+domready(function () {
 
-getUser.onclick = function () {
-  var req = new XMLHttpRequest();
-  req.onreadystatechange = function () {
-    if (req.readyState === 4 && req.status === 200) {
-      var user = JSON.parse(req.responseText);
-      results.innerText += user.firstName + ' ' + user.lastName + '\n';
-    }
+  var causeError =  document.getElementById('cause-error')
+    , getUser    =  document.getElementById('get-user')
+    , getUnknown =  document.getElementById('get-unknown')
+    , results    =  document.getElementById('results')
+
+  causeError.onclick = function () {
+    var req = new XMLHttpRequest();
+    req.open("GET", "/error");
+    req.send();
   };
-  req.open("GET", "/user");
-  req.send();
-};
 
-getUnknown.onclick = function () {
-  var req = new XMLHttpRequest();
-  req.open("GET", "/unknown");
-  req.send();
-};
+  getUser.onclick = function () {
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+      if (req.readyState === 4 && req.status === 200) {
+        var user = JSON.parse(req.responseText);
+        results.innerText += user.firstName + ' ' + user.lastName + '\n';
+      }
+    };
+    req.open("GET", "/user");
+    req.send();
+  };
+
+  getUnknown.onclick = function () {
+    var req = new XMLHttpRequest();
+    req.open("GET", "/unknown");
+    req.send();
+  };
+});
