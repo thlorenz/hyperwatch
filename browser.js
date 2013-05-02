41e7b535e691d9571d47192bcba33a1f2fd74915
Thorsten Lorenz <thlorenz@gmx.de>
2013-05-01 22:49:57 -0400

    initializing terminals on domready

--- browser.js
+++ browser.js
@@ -1,10 +1,11 @@
 'use strict';
 /*jshint browser: true */
 
-var shoe = require('shoe')
-  , hypernal = require('hypernal')
-  , getStyleProperty = require('get-style-property')
-  , loadStyles = require('./load-styles')
+var shoe             =  require('shoe')
+  , hypernal         =  require('hypernal')
+  , getStyleProperty =  require('get-style-property')
+  , loadStyles       =  require('./load-styles')
+  , domready         =  require('domready')
   ;
 
 function addWatch(classname) {
@@ -19,40 +20,44 @@ function addWatch(classname) {
   return { container: container, term: term, child: container.children[0] };
 }
 
-loadStyles();
+function init () {
+  loadStyles();
 
-var mini = addWatch('hyperwatch-mini')
-  , full = addWatch('hyperwatch-full');
+  var mini = addWatch('hyperwatch-mini')
+    , full = addWatch('hyperwatch-full');
 
-mini.container.onclick = function () { 
-  var current = getStyleProperty(full.container, 'display');
+  mini.container.onclick = function () { 
+    var current = getStyleProperty(full.container, 'display');
 
-  var next = current === 'none' ? 'block' : 'none';
-  full.container.style.setProperty('display', next);
+    var next = current === 'none' ? 'block' : 'none';
+    full.container.style.setProperty('display', next);
 
-  // Makes sense to start tailing every time we open full terminal and immediately scroll to bottom via empty write
-  if (next === 'block') { 
-    full.term.tail = true;
-    full.term.write('');
-  }
-};
+    // Makes sense to start tailing every time we open full terminal and immediately scroll to bottom via empty write
+    if (next === 'block') { 
+      full.term.tail = true;
+      full.term.write('');
+    }
+  };
 
-window.c = full.container;
-full.container.onscroll = function () {
-  var containerBottom = full.container.getBoundingClientRect().bottom
-    , childBottom = full.child.getBoundingClientRect().bottom;
+  window.c = full.container;
+  full.container.onscroll = function () {
+    var containerBottom = full.container.getBoundingClientRect().bottom
+      , childBottom = full.child.getBoundingClientRect().bottom;
 
-  full.term.tail = childBottom - containerBottom < 10;
-};
+    full.term.tail = childBottom - containerBottom < 10;
+  };
 
-window.mini = mini;
-window.full = full;
+  window.mini = mini;
+  window.full = full;
 
-var stderr = shoe('/stderr');
-var stdout = shoe('/stdout');
+  var stderr = shoe('/stderr');
+  var stdout = shoe('/stdout');
 
-stderr.pipe(mini.term);
-stderr.pipe(full.term);
+  stderr.pipe(mini.term);
+  stderr.pipe(full.term);
 
-stdout.pipe(mini.term);
-stdout.pipe(full.term);
+  stdout.pipe(mini.term);
+  stdout.pipe(full.term);
+}
+
+domready(init);
