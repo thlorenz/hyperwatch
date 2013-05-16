f0f8504a67d9eaeb673b096403e5aec6b1f9f2af
Thorsten Lorenz <thlorenz@gmx.de>
2013-05-16 08:25:07 -0700

    buffering previously logged messages
    - on page refresh previously logged messages write to the browser again
    - thus log is preserved between refreshes

--- index.js
+++ index.js
@@ -1,28 +1,53 @@
 'use strict';
 
-var shoe = require('shoe');
+var maxbuflen = 500;
+var shoe = require('shoe')
+  , errStream
+  , outStream
+  , buf = [];
 
-var stderr = shoe(function (stream) {
+function buffer(args) {
+  buf.push(args);
+  if (buf.length > maxbuflen) buf.shift();
+}
+
++function redirectStderr () {
   var stderr = process.stderr;
   var stderr_write = stderr.write;
 
   stderr.write = function () {
     stderr_write.apply(stderr, arguments);
-    stream.write.apply(stream, arguments);
+    if (errStream) errStream.write.apply(errStream, arguments);
+    buffer(arguments);
   };
-});
+}();
 
-var stdout = shoe(function (stream) {
++function redirectStdout () {
   var stdout = process.stdout;
   var stdout_write = stdout.write;
-
   stdout.write = function () {
     stdout_write.apply(stdout, arguments);
-    stream.write.apply(stream, arguments);
+    if (outStream) outStream.write.apply(outStream, arguments);
+    buffer(arguments);
   };
+}();
+
+var stderrSock = shoe(function (stream) {
+
+  if (errStream) errStream.destroy();
+  errStream = stream;
+
+  buf.forEach(function (args) { 
+    errStream.write.apply(errStream, args); 
+  });
+});
+
+var stdoutSock = shoe(function (stream) {
+  if (outStream) outStream.destroy();
+  outStream = stream;
 });
 
 module.exports = function (installee) {
-  stderr.install(installee, '/stderr');
-  stdout.install(installee, '/stdout');
+  stderrSock.install(installee, '/stderr');
+  stdoutSock.install(installee, '/stdout');
 };
